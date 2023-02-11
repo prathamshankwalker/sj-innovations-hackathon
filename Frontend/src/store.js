fb413d4ee74ce9b6966f22d5d33da3adaa5952fb
Sachin <sachin@pop-os.localdomain>
2023-02-11 10:02:44 +0530

    resolve merge

--- Frontend/src/store.js
+++ Frontend/src/store.js
@@ -3,15 +3,11 @@ import { userReducer } from "./reducers/users";
 import { projectReducer } from "./reducers/project";
 import { detailReducer } from "./reducers/details";
 import { superLeaveReducer } from "./reducers/superLeave";
-// import {allMedsReducer} from './reducers/meds'
-// import {allSalesReducer,cartsReducer} from './reducers/sales'
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     project: projectReducer,
-    Detail: detailReducer,
-    superLeaves: superLeaveReducer,
   },
 });
 export default store;
