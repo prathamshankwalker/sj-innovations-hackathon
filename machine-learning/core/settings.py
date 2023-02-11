8ba98083d68397561b0cf0b3f38a328b3cd8f131
Atharva Parkhe <patharv777@gmail.com>
2023-02-11 15:17:04 +0530

    cors

--- machine-learning/core/settings.py
+++ machine-learning/core/settings.py
@@ -59,4 +59,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:5173"]
+CORS_ALLOW_ALL_ORIGINS = True
